7b37ad5028bd000011365e063358560f0ef2a1b2
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-04-01 10:11:35 +0530

    Issue #SB-17875 feat: Updated hardDiskInfo for windows

--- server/utils/hardDiskInfo.ts
+++ server/utils/hardDiskInfo.ts
@@ -3,20 +3,22 @@ import { containerAPI } from "OpenRAP/dist/api";
 import * as os from "os";
 import { manifest } from "../manifest";
 const systemSDK = containerAPI.getSystemSDKInstance(manifest.id);
+const fileSDK = containerAPI.getFileSDKInstance(manifest.id);
 
 export default class HardDiskInfo {
     public static async getAvailableDiskSpace() {
         const { availableHarddisk, fsSize } = await systemSDK.getHardDiskInfo();
+
         if (os.platform() === "win32") {
             const fileSize: any = fsSize;
-            // tslint:disable-next-line:no-string-literal
-            const totalHarddisk = _.find(fileSize, { mount: "C:" })["size"] || 0;
-            // tslint:disable-next-line:no-string-literal
-            const usedHarddisk = _.find(fileSize, { mount: "C:" })["used"] || 0;
-            const availableHardDisk = totalHarddisk - usedHarddisk;
-            return availableHardDisk - 300000000; // keeping buffer of 300 mb, this can be configured
+            const getAvailableSpace = (drive: any) => drive.size - drive.used;
+            const currentContentPath = fileSDK.getAbsPath("") || "C:";
+            const selectedDrive = fileSize.find((driveInfo) => currentContentPath.startsWith(driveInfo.fs));
+            const availableDriveSpace = getAvailableSpace(selectedDrive);
+
+            return availableDriveSpace - 5e+8; // keeping buffer of 500 mb, this can be configured
         } else {
-            return availableHarddisk - 300000000; // keeping buffer of 300 mb, this can be configured
+            return availableHarddisk - 5e+8; // keeping buffer of 500 mb, this can be configured
         }
     }
 }
